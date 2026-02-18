9c6222039f6c4e8100d377dba59de771d43ce766
lmartin26404 <lukemartin3507@gmail.com>
2026-02-17 21:23:14 -0500

    Backend Python Dashboard

---
+++ PythonDashboard.py
@@ -0,0 +1,186 @@
+# All the imports
+import MaybeObject as ml
+
+import sys
+import pandas as pd
+import mysql.connector
+
+# Gets the object that is selected by the user in a dropdown menu.
+option = sys.argv[1]
+
+# MySQL connections
+connection = mysql.connector.connect(user='root',password='Password@MySQL',host = 'localhost', database = 'songDatabase', ssl_disabled=True)
+
+cursor = connection.cursor()
+
+# The graphs
+if(option == "one"):
+    object = sys.argv[2]
+    artist = sys.argv[3]
+    group = sys.argv[4]
+
+    # Delete the old data from the table.
+    
+    if(group != "group"):
+        cursor.execute("delete from songs_return;")
+
+    nosplit = artist
+    artist = artist.strip()
+    
+    # The query that is going to be ran
+    if nosplit == "all":
+        cursor.execute("SELECT * FROM songs_data;")
+    else:
+        cursor.execute("SELECT * FROM songs_data where artist = %s;", (artist,))
+
+    # The object to look for
+    if object == "gender":
+        objectArray = ['girl','boy','man','dude','girl','women','male','female','lady']
+    elif object == "direction":
+        objectArray = ['North', 'East', 'South', 'West', 'Left', 'Right', 'Up', 'Down']
+    elif object == "response":
+        objectArray = ['yes','yeah','no','nope','sure','alright']
+    elif object == "solar":
+        objectArray =  ['Saturn','Jupiter','Neptune','Uranus','Mercury','Venus','Mars','Earth','Pluto','milky way','sky','moon']
+    elif object == "day":
+        objectArray = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+    elif object == "month":
+        objectArray = ['January','February','March','April','May','June','July','August','September','October','Novemeber','Decemeber']
+    elif object == "drinks":
+        objectArray = ['water','tea','coffee','milk','soda','pop','juice','milkshake','beer','wine','vodka']
+    elif object == "cars":
+        objectArray = ['volvo','volkswagen','toyota','ford','bmw','kia','audi','saab','nissan','hyundai','chevroley','mazda','tesla','subaru','mitsubishi','honda','porche','lexus','jeep','chrysler','dodge','jaguar','cadillac','dodge','cadillac','pontiac','buick','lincoln','mercury','ferrari','bently','lamborghini']
+    elif object == "relationship":
+        objectArray = ['love','kiss','boyfriend','girlfriend','relationship','hug','babe','wedding','marry','breakup','baby','heart','fiancee']
+
+    # Add the query data into an array
+    results = []
+    for i, data in enumerate(cursor):
+        results.append(data)
+
+    # Converts the array into a dataframe with the right column headers.
+    df_pulled = pd.DataFrame(results)
+    df_pulled.columns = ["id","title","album","year","genre","artist","lyrics"]
+
+    # Creates an empty dictionary used to store the Object Word followed by the Count
+    word_count = {}
+    lyric_count = {}
+
+    # Loop through the dataframe to find different information
+    counter = 0
+    for index, row in df_pulled.iterrows():
+        line = row["lyrics"]
+     
+        for lyric_line in line.split("\n"):
+
+            # Prints the current line
+            if ["May","may"] in lyric_line.split():
+
+                if lyric_line not in lyric_count:
+                    lyric_count[lyric_line] = 1
+                else:
+                    lyric_count[lyric_line] += 1
+
+                ml.main(lyric_line, "May")
+               
+            for word in lyric_line.split():
+                cleanWord = word.replace(",","")
+
+                #if "may" in lyric_line:
+                    # Calls a ML function to check if the word is the true object or not.
+                    
+
+              
+                # Checks for the counts
+                if cleanWord in objectArray:
+                    if cleanWord not in word_count:
+                        word_count[cleanWord] = 1
+                    else:
+                        word_count[cleanWord] += 1
+            
+    # Adds the dictionary to a SQL database
+    for key_col, value_col in word_count.items():
+        cursor.execute("INSERT INTO songs_return (key_col, value_col, artist) VALUES (%s, %s, %s)", (key_col, value_col, ''.join(artist)))
+
+    connection.commit()
+
+# The key words
+if(option == "two"):
+    # The index of the selected value.
+    numIndex = sys.argv[2]
+    numIndex = int(numIndex)
+
+    # Include stop words or not
+    stopWords = sys.argv[3]
+    stopWords = int(stopWords)
+
+    # Array of stop words
+    stopWordsArray = ["i","me","my","myself","we","our","ours","ourselves","you","your","yours","yourself","yourselves","he","him","his","himself","she","her","hers","herself","it","its","itself","they","them","their","theirs","themselves","what","which","who","whom","this","that","these","those","am","is","are","was","were","be","been","being","have","has","had","do","does","did","doing","a","an","the","and","but","if","or","because","as","until","while","of","at","by","for","with","about","against","between","into","through","during","before","after","above","below","to","from","up","down","in","out","on","off","over","under","again","further","then","once","here","there","when","where","why","how","all","any","both","each","few","more","most","other","some","such","no","nor","not","only","own","same","so","than","too","can","will","just","don","should", "i'm", "know","don't","it's"]
+
+    # Need to go through all the songs and word by word to word. The input is the slicer value which is the word index
+    cursor.execute("SELECT * from songs_data")
+
+    # Adds the songs lyrics into an array
+    results = []
+    for i, data in enumerate(cursor):
+        results.append(data)
+
+    # Converts the array into a dataframe with the right column headers.
+    df_pulled = pd.DataFrame(results)
+    df_pulled.columns = ["id","title","album","year","genre","artist","lyrics"]
+
+    word_count = {}
+
+    currentWord = ""
+
+    # Loop through the song lyrics to word by word
+    for index, row in df_pulled.iterrows():
+        
+        # Gets the lyrics
+        line = row["lyrics"]
+        line = line.lower()
+
+        # Splits the lyrics by word
+    
+        word = line.split()
+
+        currentWord = currentWord.lower()
+
+      
+        # Error handling to prevent an out of bounds error
+        try:
+            currentWord = word[numIndex]
+
+        except IndexError:
+            currentWord = "END OF SONG"
+        
+        if currentWord not in word_count:
+            if stopWords == 1:
+                # 1: Do not add stop words
+                if currentWord not in stopWordsArray:
+                    word_count[currentWord] = 1
+            if stopWords == 0:
+                # 0: Add stop words
+                word_count[currentWord] = 1
+        else:
+            if currentWord in word_count:
+                word_count[currentWord] += 1    
+
+    # Sort the dictionary
+    sort_dict = dict(sorted(word_count.items(),key=lambda item:item[1],reverse=True))
+    
+    print(sort_dict)
+
+    # Delete the old data from the table.
+    cursor.execute("delete from songs_return;")
+
+    # Adds the dictionary to a SQL datbase to be
+    for key_col, value_col in sort_dict.items():
+        cursor.execute("INSERT INTO songs_return (key_col, value_col) VALUES (%s, %s)", (key_col, value_col))
+
+    connection.commit()
+
+
+# Closes the connection
+cursor.close()
+connection.close()
